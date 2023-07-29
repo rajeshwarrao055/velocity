b3145ce5094621ed3b86d2c32dad2417973a1b6b
Rajeshwar Rao <rajeshwarrao055@gmail.com>
2023-07-29 06:49:04 +0530

    Rajeshwar | Adding JAXB Class generation logic as part of build (#2)

--- src/main/java/org/personal/Main.java
+++ src/main/java/org/personal/Main.java
@@ -1,7 +1,11 @@
 package org.personal;
+import org.personal.Person;
 
 public class Main {
     public static void main(String[] args) {
+        Person person = new Person();
+        person.setAge(23);
+        System.out.println(person.getAge());
         System.out.println("Hello world!");
     }
 }
